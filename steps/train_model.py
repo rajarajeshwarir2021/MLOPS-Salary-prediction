61af8d2e4acf758eda74faeac05df01c0bd6ade8
rajar <rajarajeshwarir2021@gmail.com>
2024-01-22 13:08:18 +0100

    Updated train_model.py

--- steps/train_model.py
+++ steps/train_model.py
@@ -1,11 +1,20 @@
 import logging
+import mlflow
 import numpy as np
 from sklearn.base import RegressorMixin
 from zenml import step
+from zenml.client import Client
 
 from src.develop_model import RandomForestRegressorModel
+from src.read_config import ReadConfig
 
-@step
+config = ReadConfig(config_path="config/params.yaml")
+params = config.read_params()
+model_name = params['model']['model_name']
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(X_train: np.ndarray, y_train: np.ndarray, config_params: object) -> RegressorMixin:
     """
     Create and Train a Regressor model with the ingested data.
@@ -17,6 +26,7 @@ def train_model(X_train: np.ndarray, y_train: np.ndarray, config_params: object)
     try:
         model = None
         if config_params['model']['model_name'] == "RandomForestRegression":
+            mlflow.sklearn.autolog()
             random_state = config_params["base"]["random_state"]
             n_estimators = config_params["estimators"]["RandomForestRegressor"]["params"]["n_estimators"]
             model = RandomForestRegressorModel().train(X_train, y_train, n_estimators, random_state)
